7e52bdeb83b5924d33bdf6f620d16c0daee52834
lekrau <206306997+lekrau@users.noreply.github.com>
2026-07-11 14:39:49 +0200

    Add close button to image overlay

--- gallery.js
+++ gallery.js
@@ -4,18 +4,29 @@
 const showOverlay = event => {
     const target = event.target;
     const source = target.getAttribute("src");
-    console.log("target", target);
-    console.log("image", source);
+    // console.log("target", target);
+    // console.log("image", source);
 
     const skibussGallery = document.querySelector(".skibuss-gallery");
     const overlay = document.createElement("div");
     const image = document.createElement("img");
+    const close = document.createElement("button");
+
     //  setAttribute is a HTML injection sink?
     image.setAttribute("src", source);
     overlay.classList.add("overlay");
+    close.classList.add("close");
+    close.textContent = "x";
+    close.addEventListener("click", closeOverlay);
+
     skibussGallery.appendChild(overlay);
     overlay.appendChild(image);
-}
+    overlay.appendChild(close);
+};
+
+const closeOverlay = () => {
+    console.log("Close button clicked!");  
+};
 
 // MAIN
 const images = document.querySelectorAll(".skibuss-gallery img");
