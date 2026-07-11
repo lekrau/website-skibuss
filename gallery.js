c6d411c54c9e23a882eafb5ef05804392120671e
lekrau <206306997+lekrau@users.noreply.github.com>
2026-07-11 12:04:52 +0200

    Identify clicked gallery images

--- gallery.js
+++ gallery.js
@@ -1,20 +1,11 @@
 "use strict";
-const section = document.querySelector(".section");
-const para1 = document.querySelector(".para-1");
-const para2 = document.querySelector(".para-2");
-const para3 = document.querySelector("p");
-console.log(document);
-console.log(section);
-console.log(para1);
-console.log(para2);
-// alert("hello w0rld!");
 
+const images = document.querySelectorAll(".skibuss-gallery img");
+console.log(images);
 
-const button = document.querySelector(".skibuss-gallery .button");
-console.log(button);
-button.addEventListener("click", event => {
-    const newPara = document.createElement("p");
-    newPara.textContent = "Para";
-    const skibussGallery = document.querySelector(".skibuss-gallery");
-    skibussGallery.appendChild(newPara);
-});
+images.forEach(image => image.addEventListener("click", event => {
+    const target = event.target;
+    const src = target.getAttribute("src");
+    console.log(target);
+    console.log(src);
+}));
